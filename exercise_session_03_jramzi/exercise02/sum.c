86e79b7bb75c21850c9b9fee6713125ad5d0b9ee
fufess <joseph.ramzifarage@uzh.ch>
2022-03-17 22:42:49 +0100

    exercise 2

---
+++ exercise_session_03_jramzi/exercise02/sum.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <math.h>
+#include "getTime.h"
+
+typedef struct {
+    float x;
+    float y;
+    float z;
+    float r2;
+} coord;
+
+int main(int argc, char *argv[]) {
+    double tstart = getTime();
+    const int N = 1024*1024*256;
+    coord *data;
+    int i;
+    double sum;
+
+    data = malloc(N * sizeof(coord));
+    assert(data);
+
+    sum = 0.0;
+    
+    #pragma omp parallel for reduction(+ : sum)
+
+
+    for(i=0; i<N; ++i) {
+        data[i].x = i & 31;
+        data[i].y = i & 63;
+        data[i].z = i & 15;
+        data[i].r2 = data[i].x*data[i].x + data[i].y*data[i].y + data[i].z*data[i].z;
+        sum += sqrt(data[i].r2);
+    }
+    double tend = getTime();
+    double time = tend - tstart;
+    printf("sum=%f\n", sum);
+    printf("Time to execute: %.4g seconds\n", time);
+    return 0;
+}
