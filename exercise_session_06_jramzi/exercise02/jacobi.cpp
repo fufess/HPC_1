2b27fbde9c895e1cc86185ffc08cb066fbec9de4
fufess <joseph.ramzifarage@uzh.ch>
2022-04-07 23:23:57 +0200

    ex 2 v2

--- exercise_session_06_jramzi/exercise02/jacobi.cpp
+++ exercise_session_06_jramzi/exercise02/jacobi.cpp
@@ -17,8 +17,8 @@ double norm_diff(params p, double** mat1, double** mat2){
 
     #pragma omp parallel for
     for (int i=0; i<p.nx; i++){
+	#pragma omp parallel for reduction(+:sum)
     	for (int j=0; j<p.ny; j++){
-		#pragma omp atomic
         	sum += (mat1[i][j] - mat2[i][j])*(mat1[i][j] - mat2[i][j]);
     	}	
     }
@@ -44,15 +44,13 @@ void jacobi_step(params p, double** u_new, double** u_old, double** f){
     #pragma omp parallel for    
     for (int i=0; i<p.nx; i++){
         for (int j=0; j<p.ny; j++){
-	    #pragma omp atomic
             u_old[i][j] = u_new[i][j];
 	}
     }
 
     #pragma omp parallel for
     for (int i=1; i<p.nx-1; i++){
-        for (int j=1; j<p.ny-1; j++){
-	    #pragma omp atomic
+        for (int j=1; j<p.ny-1; j++){	    
             u_new[i][j] = 0.25*(u_old[i-1][j] + u_old[i+1][j] + u_old[i][j-1] + u_old[i][j+1] - dx*dy*f[i][j]);
 	}
     }
