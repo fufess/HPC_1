2e03487a6be6f7e0e9430509e1b2163685b62ca5
fufess <100378899+fufess@users.noreply.github.com>
2022-04-07 19:56:02 +0200

    Update jacobi.cpp

--- exercise_session_06_jramzi/exercise02/jacobi.cpp
+++ exercise_session_06_jramzi/exercise02/jacobi.cpp
@@ -17,8 +17,8 @@ double norm_diff(params p, double** mat1, double** mat2){
 
     #pragma omp parallel for
     for (int i=0; i<p.nx; i++){
-	#pragma omp atomic
     	for (int j=0; j<p.ny; j++){
+		#pragma omp atomic
         	sum += (mat1[i][j] - mat2[i][j])*(mat1[i][j] - mat2[i][j]);
     	}	
     }
@@ -43,18 +43,20 @@ void jacobi_step(params p, double** u_new, double** u_old, double** f){
     
     #pragma omp parallel for    
     for (int i=0; i<p.nx; i++){
-	#pragma omp atomic
         for (int j=0; j<p.ny; j++){
+	    #pragma omp atomic
             u_old[i][j] = u_new[i][j];
 	}
     }
 
     #pragma omp parallel for
     for (int i=1; i<p.nx-1; i++){
-	#pragma omp atomic
-        for (int j=1; j<p.ny-1; j++)
+        for (int j=1; j<p.ny-1; j++){
+	    #pragma omp atomic
             u_new[i][j] = 0.25*(u_old[i-1][j] + u_old[i+1][j] + u_old[i][j-1] + u_old[i][j+1] - dx*dy*f[i][j]);
+	}
     }
+	
     // printf("Function jacobi_step (jacobi.cpp l.26): not implemented.\n");
     if (p.nx!=p.ny) printf("In function jacobi_step (jacobi.cpp l.26): nx != ny, check jacobi updates\n");
 }
