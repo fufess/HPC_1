b0a2e8d949507cbae7bc472c2b27381c4c15c372
fufess <joseph.ramzifarage@uzh.ch>
2022-04-07 15:48:19 +0200

    exercie 1

--- exercise_session_06_jramzi/exercise01/parallel.c
+++ exercise_session_06_jramzi/exercise01/parallel.c
@@ -9,16 +9,16 @@ int read_file(char *path,int *buff,int *size){
     char line[MAX_LINE_LENGTH] = {0};
     unsigned int line_count = 0,i=0;
 
-
+    
     /* Open file */
     FILE *file = fopen(path, "r");
-
+    
     if (!file)
     {
         perror(path);
         return EXIT_FAILURE;
     }
-
+    
     /* Get each line until there are none left */
     while (fgets(line, MAX_LINE_LENGTH, file))
     {
@@ -26,9 +26,9 @@ int read_file(char *path,int *buff,int *size){
         //printf("line[%06d]: %s", ++line_count, line);
         buff[i] = atoi(line);
         i++;
-
+        
     }
-
+    
     *size = i;
     /* Close file */
     if (fclose(file))
@@ -40,37 +40,43 @@ int read_file(char *path,int *buff,int *size){
 }
 
 
+
+
+
+
 int main(int argc, char *argv[]){
 
-        int num_size, true_n0=646016;
-        int numbers[2000000];
+	int num_size, true_n0=646016;
+	int numbers[2000000];
+	
+	read_file("num.txt",numbers,&num_size);
+	printf("Size of integer array/file: %d\n",num_size);
 
-        read_file("num.txt",numbers,&num_size);
-        printf("Size of integer array/file: %d\n",num_size);
-
+    
     // first loop
     int maxval = 0;
-
+    
     #pragma omp parallel for
     for (int i=0;i<num_size;i++){
-	    if (numbers[i] > maxval){
-		    maxval = numbers[i];
-	    };
-    };
-    printf("max number in file: %d\n",maxval);
+	    #pragma omp critical
+            if (numbers[i] > maxval){
+                maxval = numbers[i];
+            };
+    }
+    printf("max number in file: %d\n",maxval);	
 
     // second loop
     int num_n0 = 0;
-
+    
     #pragma omp parallel for
     for (int i=0;i<num_size;i++){
-	    if (numbers[i] == 0){ 
-		    num_n0++;
-	    };
-	    #pragma omp atomic
+	    if (numbers[i] == 0){
+		#pragma omp atomic
+                num_n0++;
+        };
     };
-    printf("number of 0s in file: %d\n",num_n0);
-    printf("true number of 0s in file: %d\n",true_n0);
+    printf("number of 0s in file: %d\n",num_n0);  
+    printf("true number of 0s in file: %d\n",true_n0);  
 
     return 0;
 }
