73eebf9e25059e5b89fc65b7b58cef30cf6bfd0c
fufess <joseph.ramzifarage@uzh.ch>
2022-05-24 16:56:28 +0200

    ex11.1

--- exercise_session_11_jramzi/axpy/axpy_openacc.cpp
+++ exercise_session_11_jramzi/axpy/axpy_openacc.cpp
@@ -18,6 +18,7 @@ void axpy_gpu(int n, double alpha, const double *x, double* y) {
     int i;
 
     // TODO: offload this loop to the GPU
+    #pragma acc parallel loop
     for(i = 0; i < n; ++i) {
         y[i] += alpha*x[i];
     }
